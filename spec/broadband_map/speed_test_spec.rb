b974c3944ef8779cabae0ed801307aa87a49e7aa
blakeshall <blake@blakeshall.com>
2012-04-05 11:59:46 -0400

    Tests

--- spec/broadband_map/speed_test_spec.rb
+++ spec/broadband_map/speed_test_spec.rb
@@ -5,57 +5,56 @@ describe BroadbandMap::Client::SpeedTest do
   before do
     @client = BroadbandMap::Client.new
   end
-  
+
   describe ".speed_test_geo_type_id" do
     before do
       stub_get("speedtest/state/ids/01,02?format=json&speedtesttype=&callback=").
         to_return(:status => 200, :body => fixture("speed_geo_type_id.json"))
     end
-    
+
     it "should get the correct item" do
       test = @client.speed_test_geo_type_id({:geography_type => 'state', :geography_ids => ['01', '02']})
       a_get("speedtest/state/ids/01,02?format=json&speedtesttype=&callback=").should have_been_made
       test.results[0].geographyId.should == "01"
     end
   end
-  
+
   describe ".speed_test_geo_type_name" do
     before do
       stub_get("speedtest/state/names/alabama,arizona?format=json&speedtesttype=&callback=").
         to_return(:status => 200, :body => fixture("speed_geo_type_id.json"))
     end
-    
+
     it "should get the correct item" do
       test = @client.speed_test_geo_type_name({:geography_type => 'state', :geography_names => ['alabama', 'arizona']})
       a_get("speedtest/state/names/alabama,arizona?format=json&speedtesttype=&callback=").should have_been_made
       test.results[0].geographyId.should == "01"
     end
   end
-  
+
   describe ".speed_test_quartile" do
     before do
       stub_get("speedtest/state/quartile?format=json&speedtesttype=&callback=").
         to_return(:status => 200, :body => fixture("speed_quartile_type.json"))
     end
-    
+
     it "should get the correct item" do
       test = @client.speed_test_quartile({:geography_type => 'state'})
       a_get("speedtest/state/quartile?format=json&speedtesttype=&callback=").should have_been_made
       test.results.minimumLowerQuartileDownload.should == 0.00781
     end
   end
-  
+
   describe ".speed_test_nation" do
     before do
       stub_get("speedtest/nation?format=json&speedtesttype=&callback=").
         to_return(:status => 200, :body => fixture("speed_nation.json"))
     end
-    
+
     it "should get the correct item" do
       test = @client.speed_test_nation()
       a_get("speedtest/nation?format=json&speedtesttype=&callback=").should have_been_made
       test.results[0].geographyId.should == "99"
     end
   end
-  
 end
--- spec/broadband_map/wire_spec.rb
+++ spec/broadband_map/wire_spec.rb
@@ -5,31 +5,30 @@ describe BroadbandMap::Client::Wire do
   before do
     @client = BroadbandMap::Client.new
   end
-  
+
   describe ".wireless" do
-    before do 
+    before do
       stub_get("broadband/fall2010/wireless?latitude=42.456&longitude=-74.987&format=json&maxresults=100&callback=").
         to_return(:status => 200, :body => fixture("wireless.json"))
     end
-    
+
     it "should return correct item" do
       test = @client.wireless({:data_version => 'fall2010', :latitude => 42.456, :longitude => -74.987})
       a_get("broadband/fall2010/wireless?latitude=42.456&longitude=-74.987&format=json&maxresults=100&callback=").should have_been_made
       test.results.wirelessServices[0].frn.should == "0003774593"
     end
   end
-  
+
   describe ".wireline" do
-    before do 
+    before do
       stub_get("broadband/fall2010/wireline?latitude=42.456&longitude=-74.987&format=json&maxresults=100&callback=").
         to_return(:status => 200, :body => fixture("wireline.json"))
     end
-    
+
     it "should return correct item" do
       test = @client.wireline({:data_version => 'fall2010', :latitude => 42.456, :longitude => -74.987})
       a_get("broadband/fall2010/wireline?latitude=42.456&longitude=-74.987&format=json&maxresults=100&callback=").should have_been_made
       test.results.wirelineServices[0].blockFIPS.should == "360779908001025"
     end
   end
-  
 end
